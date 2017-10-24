355e00c7b7c27c59f9939fce0e851497157160fe
Michael Tiller <michael.tiller@gmail.com>
2017-10-24 17:15:16 -0400

    Using async/await

--- create_repo.ts
+++ create_repo.ts
@@ -15,6 +15,13 @@ if (!argv.repo) {
 }
 
 if (argv.tool) {
+    run();
+} else {
+    console.error("No value provided for --tool");
+    process.exit(1);
+}
+
+async function run() {
     console.log("Look for tool '" + argv.tool + "'");
     let toolFileName = `${argv.tool}.info`;
     let toolFile = path.join(SVN, "tools", toolFileName);
@@ -25,33 +32,32 @@ if (argv.tool) {
         throw new Error("No tool file named '" + toolFile + "' found");
     }
 
-    getExports(FMUs, (d) => d.exporter.tool == argv.tool).then((details) => {
-        console.log("Copying exported FMUs");
-        details.forEach((match) => {
-            let from = path.join(match.dir);
-            let to = path.join(argv.repo, "Test_FMUs", match.rel);
+    console.log("Collecting export directories for " + argv.tool);
+    let edetails = await getExports(FMUs, (d) => d.exporter.tool == argv.tool);
+    edetails.forEach((match, index) => {
+        let from = path.join(match.dir);
+        let to = path.join(argv.repo, "Test_FMUs", match.rel);
 
-            console.log(`  Copying directory '${from}' to '${to}'`)
-            fs.copySync(from, to, {
-                recursive: true,
-            });
-        })
+        let per = (100 * index / edetails.length).toFixed(0);
+        process.stdout.write(` [${per}%] \r`);
+        fs.copySync(from, to, {
+            recursive: true,
+        });
     });
 
-    console.log("Copying imported FMUs");
-    getImports(CrossChecks, (d) => d.importer.tool == argv.tool).then((details) => {
-        console.log("Copying exported FMUs");
-        details.forEach((match) => {
-            let from = path.join(match.dir);
-            let to = path.join(argv.repo, "CrossCheck_Results", match.rel);
+    console.log("Collecting import directories for " + argv.tool);
+    let idetails = await getImports(CrossChecks, (d) => d.importer.tool == argv.tool);
+    console.log("Copying exported FMUs");
+    idetails.forEach((match, index) => {
+        let from = path.join(match.dir);
+        let to = path.join(argv.repo, "CrossCheck_Results", match.rel);
 
-            console.log(`  Copying directory '${from}' to '${to}'`)
-            fs.copySync(from, to, {
-                recursive: true,
-            });
-        })
+        let per = (100 * index / idetails.length).toFixed(0);
+        process.stdout.write(` [${per}%] \r`);
+        fs.copySync(from, to, {
+            recursive: true,
+        });
     });
-} else {
-    console.error("No value provided for --tool");
-    process.exit(1);
+
+    // TODO: Create circle.yml
 }

db28407a4723cf0b968adc47e7b0b0c7ce235a60
Michael Tiller <michael.tiller@gmail.com>
2017-10-25 13:11:16 -0400

    Export cross-check results
    The only wrinkle is that it only outputs
    it for valid tools (of which there are, so far,
    very few).

--- db.ts
+++ db.ts
@@ -78,7 +78,46 @@ export async function pushFMUs(fmus: FMUTable, artifacts: string): Promise<void>
     return;
 }
 
+interface CrossCheckDocument {
+    version: string;
+    variant: string;
+    platform: string;
+    export_tool: string;
+    export_version: string;
+    import_tool: string;
+    import_version: string;
+}
+
 export async function pushCrossChecks(xc: CrossCheckTable, artifacts: string): Promise<void> {
     fs.writeFileSync(path.join(artifacts, "xc_results.json"), JSON.stringify(xc, null, 4));
+
+    let db = await mongoConnect();
+    if (!db) return;
+    let col = db.collection<ToolDocument>("cross-check");
+
+    // Write to Mongo
+    for (let i = 0; i < xc.length; i++) {
+        let result = xc[i];
+        let doc: CrossCheckDocument = {
+            version: result.version,
+            variant: result.variant,
+            platform: result.platform,
+            export_tool: result.exporter.tool,
+            export_version: result.exporter.version,
+            import_tool: result.importer.tool,
+            import_version: result.importer.version,
+        }
+        try {
+            let result = await col.updateOne(doc, doc, { upsert: true });
+            if (result.modifiedCount + result.upsertedCount != 1) {
+                let entry = JSON.stringify(result);
+                console.warn(`Number of modified documents modified for FMU '${entry}' was ${result.modifiedCount}`);
+            }
+        } catch (e) {
+            let entry = JSON.stringify(result);
+            console.error("Error while writing FMU: " + entry + ": ", e.message);
+        }
+    }
+    await db.close();
     return;
 }
